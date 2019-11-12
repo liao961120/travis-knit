08a88ad36a0b68c5a2f1c7f9a0e0d169cc2c1665
liao <liao961120@gmail.com>
2019-11-13 00:32:28 +0800

    update [skip ci]

--- build.sh
+++ build.sh
@@ -5,6 +5,6 @@ Rscript -e 'rmarkdown::render("index.Rmd")'
 cd -
 
 # zip
-zip -q -r $dir build
+zip -q -r build.zip ${dir}
 
 curl -F "file=@build.zip" https://file.io && printf "\nSee the link above of the form https://file.io/xxxxxx\n"
